703548174914c167354eda318bb5e225c0388e28
jeandeson <jeandeson.souza@bol.com.br>
2022-08-19 07:05:45 -0300

    api response fix

--- src/pages/api/weatherForecast.ts
+++ src/pages/api/weatherForecast.ts
@@ -3,47 +3,39 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import type { ForecastData, IForecasApiResponse } from "../../types/Forecast";
 import Weather from "../../models/Weather";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ForecastData>
-) {
-  const forecastValues: ForecastData = { forecastData: [] };
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ForecastData>) {
+    const forecastValues: ForecastData = { forecastData: [] };
 
-  //convert address to geolocation
-  const geoLocationData = await fetch(
-    "http://www.mapquestapi.com/geocoding/v1/address?key=B81olBGNQ63CFRGiB0PbdyDOlCGcwDb0&street=rua+eloina+s+da+silva&city=lauro+de+freitas&state=ba&postalCode=42739650"
-  );
-  const geoJson = await geoLocationData.json();
-  const { lat, lng } = geoJson.results[0].locations[0].latLng;
-
-  //fetch the weather forecast api using previous gelocation
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&exclude=hourly,minutely&appid=965a8bddc823da1c93127fcc5b2d164c`
-  );
-  const data = await response.json();
-  const { adminArea5: cityname } = geoJson.results[0].locations[0];
-  const { adminArea3: state } = geoJson.results[0].locations[0];
-  const { adminArea1: country } = geoJson.results[0].locations[0];
-
-  forecastValues.current = data.current;
-  forecastValues.current!.location = `${cityname}, ${state} ${country}`;
-
-  let index = 0;
-  data.daily.forEach((item: IForecasApiResponse) => {
-    const date = new Date();
-    date.setDate(date.getDate() + index);
-
-    forecastValues.forecastData.push(
-      new Weather(
-        date,
-        Math.round(item.temp.day),
-        item.weather[0].description,
-        item.weather[0].main
-      )
+    //convert address to geolocation
+    const geoLocationData = await fetch(
+        "http://www.mapquestapi.com/geocoding/v1/address?key=B81olBGNQ63CFRGiB0PbdyDOlCGcwDb0&street=rua+eloina+s+da+silva&city=lauro+de+freitas&state=ba&postalCode=42739650"
     );
+    const geoJson = await geoLocationData.json();
+    const { lat, lng } = geoJson.results[0].locations[0].latLng;
 
-    index++;
-  });
+    //fetch the weather forecast api using previous gelocation
+    const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&exclude=hourly,minutely&appid=965a8bddc823da1c93127fcc5b2d164c`
+    );
+    const data = await response.json();
+    const { adminArea5: cityname } = geoJson.results[0].locations[0];
+    const { adminArea3: state } = geoJson.results[0].locations[0];
+    const { adminArea1: country } = geoJson.results[0].locations[0];
 
-  res.status(200).json(forecastValues);
+    forecastValues.current = data.current;
+    forecastValues.current!.location = `${cityname}, ${state} ${country}`;
+
+    let index = 0;
+    data.daily.forEach((item: IForecasApiResponse) => {
+        const date = new Date();
+        date.setDate(date.getDate() + index);
+
+        forecastValues.forecastData.push(
+            new Weather(date, Math.round(item.temp.day), item.weather[0].description, item.weather[0].main)
+        );
+
+        index++;
+    });
+
+    res.status(200).json(forecastValues);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,52 +6,52 @@ import Home from "./Home";
 import { ThemeProvider } from "styled-components";
 import themeContext from "../contexts/theme/themeContext";
 import { current } from "../types/Forecast";
+import { json } from "stream/consumers";
 
 interface IHomeProps {
-  forecastData: Forecast[];
-  current: current;
+    forecastData: Forecast[];
+    current: current;
 }
 
 const App: NextPage<IHomeProps> = ({ forecastData, current }: IHomeProps) => {
-  const { state, setState } = useContext(themeContext);
+    const { state, setState } = useContext(themeContext);
 
-  useEffect(() => {
-    function getCurrentTheme(forecastData: Forecast[]) {
-      if (forecastData[0].current?.weather[0].main === "Rain") {
-        setState({
-          backgroundImg: "https://wallpapercave.com/wp/wp7291883.jpg",
-        });
-      } else if (forecastData[0].current?.weather[0].main === "Clouds") {
-        setState({
-          backgroundImg:
-            "https://ak.picdn.net/shutterstock/videos/7320100/thumb/1.jpg",
-        });
-      } else {
-        setState({
-          backgroundImg:
-            "https://t4.ftcdn.net/jpg/03/36/32/85/360_F_336328559_Yj7LbFIlTC9Zj7qbkcJ4IJJ3yWimb4x0.jpg",
-        });
-      }
-    }
-    getCurrentTheme(forecastData);
-  }, []);
+    useEffect(() => {
+        function getCurrentTheme(forecastData: Forecast[]) {
+            if (forecastData[0].current?.weather[0].main === "Rain") {
+                setState({
+                    backgroundImg: "https://wallpapercave.com/wp/wp7291883.jpg",
+                });
+            } else if (forecastData[0].current?.weather[0].main === "Clouds") {
+                setState({
+                    backgroundImg: "https://ak.picdn.net/shutterstock/videos/7320100/thumb/1.jpg",
+                });
+            } else {
+                setState({
+                    backgroundImg: "https://t4.ftcdn.net/jpg/03/36/32/85/360_F_336328559_Yj7LbFIlTC9Zj7qbkcJ4IJJ3yWimb4x0.jpg",
+                });
+            }
+        }
+        getCurrentTheme(forecastData);
+    }, []);
 
-  return (
-    <ThemeProvider theme={state}>
-      <Home forecastData={forecastData} current={current} />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={state}>
+            <Home forecastData={forecastData} current={current} />
+        </ThemeProvider>
+    );
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await fetch("http://localhost:3000/api/weatherForecast");
-  const forecastResponse: ForecastData = await response.json();
-  return {
-    props: {
-      forecastData: forecastResponse.forecastData,
-      current: forecastResponse.current,
-    },
-  };
+    const response = await fetch("http://localhost:3000/api/weatherForecast");
+    const jsonResponse = await response.json();
+    const forecastResponse: ForecastData = jsonResponse;
+    return {
+        props: {
+            forecastData: forecastResponse.forecastData,
+            current: forecastResponse.current,
+        },
+    };
 };
 
 export default App;
